6caa4c4ea16a98a1e47345a3365efcec32d4042d
JuttaStrickerei <office@strickerei-jutta.at>
2025-03-12 22:11:26 +0100

    issue handling homepage

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -5,24 +5,19 @@ const Hero = () => {
   const imageUrl = `${process.env.NEXT_PUBLIC_MINIO_URL}/medusa-media/DSC09710-01JKBCRRP1H777B1NRDXMJVJBB.jpg`;
   
   return (
-    <>
-      {/* Add the background image to body using a style tag */}
-      <style jsx global>{`
-        body {
-          background-image: url('${imageUrl}');
-          background-size: cover;
-          background-position: center;
-          background-repeat: no-repeat;
-          background-attachment: fixed;
-        }
-      `}</style>
-      
-      <div className="relative h-[75vh] w-full border-b border-ui-border-base">
-        <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-6">
-          <Heading level="h1">Welcome to the Store</Heading>
-        </div>
+    <div 
+      className="relative h-[75vh] w-full border-b border-ui-border-base"
+      style={{
+        backgroundImage: `url('${imageUrl}')`,
+        backgroundSize: 'cover',
+        backgroundPosition: 'center',
+        backgroundRepeat: 'no-repeat'
+      }}
+    >
+      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-6">
+        <Heading level="h1">Welcome to the Store</Heading>
       </div>
-    </>
+    </div>
   );
 };
 
