793fddc133bab6a28f9c9e0bcb162430a98dd2e5
JuttaStrickerei <office@strickerei-jutta.at>
2025-03-12 20:55:20 +0100

    adapting home page of storefront

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,9 +1,26 @@
 import { Github } from "@medusajs/icons"
 import { Button, Heading } from "@medusajs/ui"
+import Image from "next/image" // Import the Next.js Image component
 
 const Hero = () => {
+  
+  const imageUrl = `${process.env.NEXT_PUBLIC_MINIO_URL}/._IMG_2722-01JP2HDRH327VZ71YQHKPGT2D1.jpg`
+  
   return (
     <div className="h-[75vh] w-full border-b border-ui-border-base relative bg-ui-bg-subtle">
+      {/* Add the image as a background with overlay */}
+      <div className="absolute inset-0">
+        <Image 
+          src={imageUrl}
+          alt="Hero background"
+          fill
+          style={{ objectFit: "cover" }}
+          priority
+        />
+        {/* Optional overlay to ensure text readability */}
+        <div className="absolute inset-0 bg-black/30"></div>
+      </div>
+      
       <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center small:p-32 gap-6">
         <span>
           <Heading
@@ -19,7 +36,7 @@ const Hero = () => {
             Need help customizing your store?
           </Heading>
         </span>
-        <a
+        
           href="https://funkyton.com/medusajs-2-0-is-finally-here/"
           target="_blank"
         >
