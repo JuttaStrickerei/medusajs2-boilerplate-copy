2c2f4ff6e701cce3e227b6b039a0822b25d75d72
JuttaStrickerei <office@strickerei-jutta.at>
2025-03-12 22:05:39 +0100

    change homepage, remove next.js component

--- storefront/src/modules/home/components/hero/index.tsx
+++ storefront/src/modules/home/components/hero/index.tsx
@@ -1,31 +1,28 @@
-// src/modules/home/components/hero/index.tsx
 import React from "react";
 import { Heading } from "@medusajs/ui";
-import Image from "next/image";
 
 const Hero = () => {
-  // Define the image URL (hardcoded for testing)
- const imageUrl = `${process.env.NEXT_PUBLIC_MINIO_URL}/medusa-media/DSC09710-01JKBCRRP1H777B1NRDXMJVJBB.jpg`
+  const imageUrl = `${process.env.NEXT_PUBLIC_MINIO_URL}/medusa-media/DSC09710-01JKBCRRP1H777B1NRDXMJVJBB.jpg`;
   
   return (
-    <div className="h-[75vh] w-full border-b border-ui-border-base relative">
-      {/* Background image */}
-      <div className="absolute inset-0">
-        <Image 
-          src={imageUrl}
-          alt="Hero background"
-          width={1920}  // Use appropriate dimensions
-          height={1080}
-          className="w-full h-full object-cover"
-          priority
-        />
-      </div>
+    <>
+      {/* Add the background image to body using a style tag */}
+      <style jsx global>{`
+        body {
+          background-image: url('${imageUrl}');
+          background-size: cover;
+          background-position: center;
+          background-repeat: no-repeat;
+          background-attachment: fixed;
+        }
+      `}</style>
       
-      {/* Content */}
-      <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-6">
-        <Heading level="h1">Welcome to the Store</Heading>
+      <div className="relative h-[75vh] w-full border-b border-ui-border-base">
+        <div className="absolute inset-0 z-10 flex flex-col justify-center items-center text-center gap-6">
+          <Heading level="h1">Welcome to the Store</Heading>
+        </div>
       </div>
-    </div>
+    </>
   );
 };
 
