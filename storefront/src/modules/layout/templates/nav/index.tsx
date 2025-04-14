5fb217e47078f29cdbf3d0fe8a16c9909fd626de
JuttaStrickerei <office@strickerei-jutta.at>
2025-04-14 15:18:25 +0200

    adapted the cart button in the header for translation

--- storefront/src/modules/layout/templates/nav/index.tsx
+++ storefront/src/modules/layout/templates/nav/index.tsx
@@ -1,14 +1,12 @@
-import { Suspense } from "react"
-
-import { listRegions } from "@lib/data/regions"
-import { StoreRegion } from "@medusajs/types"
-import LocalizedClientLink from "@modules/common/components/localized-client-link"
-import CartButton from "@modules/layout/components/cart-button"
-import SideMenu from "@modules/layout/components/side-menu"
-import ClientLanguageSwitcher from "../../../../i18n/components/ClientLanguageSwitcher" // Add this import
+import { listRegions } from "@lib/data/regions";
+import { StoreRegion } from "@medusajs/types";
+import LocalizedClientLink from "@modules/common/components/localized-client-link";
+import CartButton from "@modules/layout/components/cart-button";
+import SideMenu from "@modules/layout/components/side-menu";
+import NavLinks from "./nav-links";
 
 export default async function Nav() {
-  const regions = await listRegions().then((regions: StoreRegion[]) => regions)
+  const regions = await listRegions().then((regions: StoreRegion[]) => regions);
 
   return (
     <div className="sticky top-0 inset-x-0 z-50 group">
@@ -31,46 +29,13 @@ export default async function Nav() {
           </div>
 
           <div className="flex items-center gap-x-6 h-full flex-1 basis-0 justify-end">
-            <div className="hidden small:flex items-center gap-x-6 h-full">
-              {/* Add the ClientLanguageSwitcher here */}
-              <div className="hover:text-ui-fg-base">
-                <ClientLanguageSwitcher />
-              </div>
-              
-              {process.env.NEXT_PUBLIC_FEATURE_SEARCH_ENABLED && (
-                <LocalizedClientLink
-                  className="hover:text-ui-fg-base"
-                  href="/search"
-                  scroll={false}
-                  data-testid="nav-search-link"
-                >
-                  Search
-                </LocalizedClientLink>
-              )}
-              <LocalizedClientLink
-                className="hover:text-ui-fg-base"
-                href="/account"
-                data-testid="nav-account-link"
-              >
-                Account
-              </LocalizedClientLink>
-            </div>
-            <Suspense
-              fallback={
-                <LocalizedClientLink
-                  className="hover:text-ui-fg-base flex gap-2"
-                  href="/cart"
-                  data-testid="nav-cart-link"
-                >
-                  Cart (0)
-                </LocalizedClientLink>
-              }
-            >
-              <CartButton />
-            </Suspense>
+            <NavLinks />
+            
+            {/* No Suspense boundary needed */}
+            <CartButton />
           </div>
         </nav>
       </header>
     </div>
-  )
+  );
 }
