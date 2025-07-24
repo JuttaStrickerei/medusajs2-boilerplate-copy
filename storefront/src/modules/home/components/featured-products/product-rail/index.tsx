4e6baff9db7eba7bb07677a140b9e715de90cf20
Andreas Strobl <office@strickerei-jutta.at>
2025-07-24 15:24:49 +0200

    changed featured product text field for -view all-

--- storefront/src/modules/home/components/featured-products/product-rail/index.tsx
+++ storefront/src/modules/home/components/featured-products/product-rail/index.tsx
@@ -4,6 +4,8 @@ import { Text } from "@medusajs/ui"
 import InteractiveLink from "@modules/common/components/interactive-link"
 import ProductPreview from "@modules/products/components/product-preview"
 
+import { useTranslations } from "next-intl"
+
 export default function ProductRail({
   collection,
   region,
@@ -17,12 +19,14 @@ export default function ProductRail({
     return null
   }
 
+  const t = useTranslations("Homepage")
+
   return (
     <div className="content-container py-12 small:py-24">
       <div className="flex justify-between mb-8">
         <Text className="txt-xlarge">{collection.title}</Text>
         <InteractiveLink href={`/collections/${collection.handle}`}>
-          View all
+          {t("viewall")}
         </InteractiveLink>
       </div>
       <ul className="grid grid-cols-2 small:grid-cols-3 gap-x-6 gap-y-24 small:gap-y-36">
